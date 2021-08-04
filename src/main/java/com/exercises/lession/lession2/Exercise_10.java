c14e876c7e74310775d3c75ca229d8406781e55c
Vu Nguyen <vunht@onemount.com>
2021-08-04 21:26:25 +0700

    Update

--- src/main/java/com/exercises/lession/lession2/Exercise_10.java
+++ src/main/java/com/exercises/lession/lession2/Exercise_10.java
@@ -10,9 +10,9 @@ public class Exercise_10 {
 		int a = 123;
 		int b = 456;
 
-		int min = (a < b) ? a : b;
+		int min = (a <= b) ? a : b;
 
-		int max = (a > b) ? a : b;
+		int max = (a >= b) ? a : b;
 
 		System.out.println("Min = " + min);
 		System.out.println("Max = " + max);
