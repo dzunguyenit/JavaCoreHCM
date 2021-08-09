edf97ff20e4a5f197b180d17aa300bd726470c72
Vu Nguyen <vunht@onemount.com>
2021-08-09 16:40:54 +0700

    update exercise lession 04

---
+++ src/main/java/com/exercises/lession/lession4/Exercise_02.java
@@ -0,0 +1,63 @@
+package com.exercises.lession.lession4;
+
+import java.util.Scanner;
+
+public class Exercise_02 {
+
+	// Nhập một số nguyên dương n (n > 0).
+	// Hãy cho biết:
+	// S = 1*2*...*n = n!
+
+	public static long getFactorialNumber(int n) {
+		if (n == 0)
+			return 0;
+		int sum = 1;
+		for (int i = 2; i <= n; i++) {
+			sum = sum *= i;
+
+		}
+		return sum;
+	}
+
+	// Nhập một số nguyên dương n (n > 0).
+	// Hãy cho biết:
+	// S = 1! + 2! + ... + n!
+
+	public static long sumFactorial(int n) {
+		if (n == 0)
+			return 0;
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += getFactorialNumber(i);
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("--------Exercise_02_d--------");
+		System.out.println("Input n: ");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		long factorial = getFactorialNumber(n);
+
+		System.out.println(n + "!= " + factorial);
+
+		System.out.println("--------Exercise_02_e--------");
+		System.out.println("Input n: ");
+
+		int number = scanner.nextInt();
+
+		for (int i = 1; i <= number; i++) {
+			if (i == number) {
+				System.out.print(i + "! ");
+			} else {
+				System.out.print(i + "! + ");
+			}
+		}
+
+		System.out.println("= " + sumFactorial(number));
+
+	}
+
+}
