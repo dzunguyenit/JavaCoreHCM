f6e295f8dad5554abccf9935f358507ba79b6d65
vunguyen <dzunguyenit@gmail.com>
2021-08-18 09:45:29 +0700

    Updatge

--- src/main/java/com/exercises/lession/lession8/Exercise_01_02_03.java
+++ src/main/java/com/exercises/lession/lession8/Exercise_01_02_03.java
@@ -70,7 +70,8 @@ public class Exercise_01_02_03 {
 		if (n <= 1) {
 			return false;
 		}
-		for (int i = 2; i <= Math.sqrt(n); i++) {
+		int squareRoot = (int) Math.sqrt(n);
+		for (int i = 2; i <= squareRoot; i++) {
 			if (n % i == 0) {
 				return false;
 			}
