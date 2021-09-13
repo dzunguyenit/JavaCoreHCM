6dda6241f5bcdd877fbcf12dd54fc82ca6d6efc1
vunguyen <dzunguyenit@gmail.com>
2021-09-13 17:37:51 +0700

    U[date

--- src/main/java/com/exercises/lession/lession14/entities/App.java
+++ src/main/java/com/exercises/lession/lession14/entities/App.java
@@ -32,6 +32,6 @@ public class App {
 			total += pricePerUnit * s.calculateArea();
 		}
 
-		System.out.println("Total pay for realstate is = " + total);
+		System.out.println("Total pay for realestate is = " + total);
 	}
 }
